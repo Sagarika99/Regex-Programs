6a51bdb81780ef53d51cd12a8d6fc93e3e0762f4
Sagarika Shinde <sagarika.shinde@morningstar.com>
2021-08-27 21:17:28 +0530

    [sagarika]: Space added between pincode number

--- src/com/regexprograms/PinCodeUC1.java
+++ src/com/regexprograms/PinCodeUC1.java
@@ -6,8 +6,8 @@ import java.util.regex.Pattern;
 public class PinCodeUC1 {
 
 	public static void main(String[] args) {
-		Pattern pattern = Pattern.compile("(^[0-9]{6}$[^A-Z])");
-		Matcher matcher = pattern.matcher("Ab400088");
+		Pattern pattern = Pattern.compile("(^[0-9]{3}\\s{0,1}[0-9]{3}$)");
+		Matcher matcher = pattern.matcher("400 088");
 		
 		boolean matchFound = matcher.find();
 			if(matchFound) {
