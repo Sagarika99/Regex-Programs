44007cc4d4f584ebbb1d08d6efc4e8d1a9671756
Sagarika Shinde <sagarika.shinde@morningstar.com>
2021-08-27 21:22:39 +0530

    [sagarika]:400 008 is valid along 400088

--- src/com/regexprograms/PinCodeUC1.java
+++ src/com/regexprograms/PinCodeUC1.java
@@ -6,8 +6,8 @@ import java.util.regex.Pattern;
 public class PinCodeUC1 {
 
 	public static void main(String[] args) {
-		Pattern pattern = Pattern.compile("(^[0-9]{3}\\s{0,1}[0-9]{3}$)");
-		Matcher matcher = pattern.matcher("400 088");
+		Pattern pattern = Pattern.compile("(^[0-9]{3}\\s{0,1}[0-9]{3}$)"); // 400 008 is also valid along with 400088
+		Matcher matcher = pattern.matcher("400088");
 		
 		boolean matchFound = matcher.find();
 			if(matchFound) {
