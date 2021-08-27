eeef17c0dac80904929deb9133c450655075efae
Sagarika Shinde <sagarika.shinde@morningstar.com>
2021-08-27 20:28:47 +0530

    [sagarika]: Pincode of 6 digits

---
+++ src/com/regexprograms/PinCodeUC1.java
@@ -0,0 +1,20 @@
+package com.regexprograms;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PinCodeUC1 {
+
+	public static void main(String[] args) {
+		Pattern pattern = Pattern.compile("[0-9]+");
+		Matcher matcher = pattern.matcher("400088");
+		
+		boolean matchFound = matcher.find();
+			if(matchFound) {
+		      System.out.println("Match found");
+		    }
+			else {
+		      System.out.println("Match not found");
+		    }
+	}
+}
